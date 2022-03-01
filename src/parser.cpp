834651fe9fb160d1c18dc6657ea907268e2ec454
Pierre Pinet <pipinet@emi.u-bordeaux.fr>
2022-03-01 16:19:35 +0100

    Quick fix.

--- src/parser.cpp
+++ src/parser.cpp
@@ -20,21 +20,23 @@ void TER::clean(std::string path){
     std::string line;
     std::fstream f;
     std::vector<std::string> words;
-    int switch = 0;
+    int switchos = 0;
     f.open(path);
     if (!f){
         std::cout<<"\ncouldn't load\n"<<std::endl;
     }else{
         std::cout<<"\nsuccessful load\n"<<std::endl;
     }
-    while(switch == 0){
+    std::string keyword = "NODES";
+    while(switchos == 0){
         std::getline(f,line);
         words = TER::split(line,' ');
-        if (words[0].equals("NODES")){
-            switch = 1;
+
+        if (words[0] == keyword){
+            switchos = 1;
         }
     }
-    while(switch == 1){
+    while(switchos == 1){
 
     }
 
