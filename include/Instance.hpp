2f15165ce93cf7f2cdc6476cebd621680943a83d
Pierre Pinet <pipinet@emi.u-bordeaux.fr>
2022-03-15 17:54:32 +0100

    Classe instance changée.

--- include/Instance.hpp
+++ include/Instance.hpp
@@ -1,5 +1,5 @@
-#ifndef GRAPH
-#define GRAPH
+#ifndef INSTANCE
+#define INSTANCE
 #include <iostream>
 #include <vector>
 
@@ -7,32 +7,62 @@
 class Instance{
 
  public:
-     //nb nodes
+
      int nbNodes;
+     std::vector<std::vector<int>> adjencyMatrix;
 
-     //capacity matrix, equal -1 if nodes aren't adjacent
-     std::vector<std::vector<int>> capacityMatrix;
-
-     //different services levels as described in the paper
-     int highVNFCapacity;
-     int mediumVNFCapacity;
      int lowVNFCapacity;
+     int mediumVNFCapacity;
+     int highVNFCapacity;
 
      int nbDemands;
-     int* demandsStart;
-     int* demandsEnd;
-     int* demands;
+     std::vector<int> demandsStart;
+     std::vector<int> demandsEnd;
+     std::vector<int> demands;
 
-     Instance(int nbNodes,std::vector<std::vector<int>> capacityMatrix){
+     int arcCapacity;
+     int serviceCapacity;
+
+     Instance(int nbNodes,std::vector<std::vector<int>> adjencyMatrix,
+              int lowVNFCapacity,int mediumVNFCapacity,int highVNFCapacity,
+              int nbDemands,std::vector<int> demandsStart,std::vector<int> demandsEnd,std::vector<int> demands,
+              int arcCapacity,int serviceCapacity){
+
          this->nbNodes=nbNodes;
-         this->capacityMatrix=capacityMatrix;
+         this->adjencyMatrix=adjencyMatrix;
+
+         this->highVNFCapacity=highVNFCapacity;
+         this->mediumVNFCapacity=mediumVNFCapacity;
+         this->lowVNFCapacity=lowVNFCapacity;
+
+         this->nbDemands=nbDemands;
+         this->demandsStart=demandsStart;
+         this->demandsEnd=demandsEnd;
+         this->demands=demands;
+
+         this->arcCapacity=arcCapacity;
+         this->serviceCapacity=serviceCapacity;
      }
 
      void print()
      {
        std::cout << "Network with " << nbNodes << " nodes\n";
-       std::cout << "Adjacency list with capacities:\n";
-       for(std::vector<int> line:capacityMatrix)
+
+       std::cout << "Arc capacity: " << arcCapacity << "\n";
+       std::cout << "Service capacity: " << serviceCapacity << "\n";
+
+       std::cout << "Low VNF Capacity:" << lowVNFCapacity << "\n";
+       std::cout << "Medium VNF Capacity:" << mediumVNFCapacity << "\n";
+       std::cout << "High VNF Capacity:" << highVNFCapacity << "\n";
+
+       std::cout << nbDemands << " demands:\n";
+       for(int demandIndex=0;demandIndex<nbDemands;demandIndex++)
+       {
+         std::cout << "Demand " << demandIndex << ", " << demands[demandIndex] << " from node " << demandsStart[demandIndex] << " to node " << demandsEnd[demandIndex] << "\n";
+       }
+
+       std::cout << "Adjacency list:\n";
+       for(std::vector<int> line:adjencyMatrix)
        {
          for(int arcValue:line)
          {
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 
 int main(int   argc,char *argv[])
 {
-  std::vector<std::vector<int>> matrixos = {{0,1,0},{1,0,0},{0,0,1}};
-  Instance testInstance = Instance(3,matrixos);
+  std::vector<std::vector<int>> matrixos = {{0,1,0},{1,0,1},{0,1,0}};
+  std::vector<int> demandsStart = {0,0};
+  std::vector<int> demandsEnd = {1,2};
+  std::vector<int> demands = {5,7};
+  Instance testInstance = Instance(3,matrixos,50,100,150,2,demandsStart,demandsEnd,demands,10,40);
   testInstance.print();
 
-  clean("../instances/abilene.txt");
+  //clean("../instances/abilene.txt");
 }
